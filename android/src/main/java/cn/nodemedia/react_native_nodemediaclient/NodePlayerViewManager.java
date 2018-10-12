1f1ed7b0245dcb814ff767f8e77c93d018589eba
Panagiotis Vourtsis <vourtsis_pan@hotmail.com>
2018-10-12 13:00:09 +0300

    add handler dependecy

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/NodePlayerViewManager.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/NodePlayerViewManager.java
@@ -7,6 +7,8 @@
 
 package cn.nodemedia.react_native_nodemediaclient;
 
+import android.os.Handler;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;

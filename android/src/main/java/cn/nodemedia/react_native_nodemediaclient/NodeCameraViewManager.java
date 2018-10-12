875763ef2a0e70086f578e0a80c993e385a005ff
Panagiotis Vourtsis <vourtsis_pan@hotmail.com>
2018-10-12 13:10:07 +0300

    fix on files

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/NodeCameraViewManager.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/NodeCameraViewManager.java
@@ -1,5 +1,5 @@
 //
-//  NodePlayerViewManager.java
+//  NodeCameraViewManager.java
 //
 //  Created by Mingliang Chen on 2017/11/29.
 //  Copyright © 2017年 NodeMedia. All rights reserved.
@@ -7,10 +7,8 @@
 
 package cn.nodemedia.react_native_nodemediaclient;
 
-import android.os.Handler;
-
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -20,104 +18,104 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
-import cn.nodemedia.NodePlayer;
+import cn.nodemedia.react_native_nodemediaclient.RCTNodeCameraView;
 
-public class NodePlayerViewManager extends SimpleViewManager<RCTNodePlayerView> {
-    private RCTNodePlayerView view;
 
-    private static final int COMMAND_PAUSE_ID = 1;
-    private static final String COMMAND_PAUSE_NAME = "pause";
+public class NodeCameraViewManager extends SimpleViewManager<RCTNodeCameraView> {
+
+    private static final int COMMAND_STARTPREV_ID =0;
+    private static final String COMMAND_STARTPREV_NAME = "startprev";
+    private static final int COMMAND_STOPPREV_ID = 1;
+    private static final String COMMAND_STOPPREV_NAME = "stopprev";
     private static final int COMMAND_START_ID = 2;
     private static final String COMMAND_START_NAME = "start";
     private static final int COMMAND_STOP_ID = 3;
     private static final String COMMAND_STOP_NAME = "stop";
-
-    public NodePlayerViewManager(ReactApplicationContext reactContext) {
-        Handler mainHandler = new Handler(reactContext.getMainLooper());
-        mainHandler.post(new Runnable() {
-            ReactApplicationContext reactContext;
-            @Override
-            public void run() {
-                view = new RCTNodePlayerView(reactContext);
-            }
-            public Runnable init(ReactApplicationContext reactContextTemp) {
-                this.reactContext = reactContextTemp;
-                return(this);
-            }
-        }.init(reactContext));
-    }
-
+    private static final int COMMAND_SWITCH_CAM_ID = 4;
+    private static final String COMMAND_SWITCH_CAM_NAME = "switchCamera";
+    private static final int COMMAND_SWITCH_FLASH_ID = 5;
+    private static final String COMMAND_SWITCH_FLASH_NAME = "flashEnable";
     @Override
     public String getName() {
-        return "RCTNodePlayer";
+        return "RCTNodeCamera";
     }
 
     @Override
-    public RCTNodePlayerView createViewInstance(ThemedReactContext reactContext) {
+    protected RCTNodeCameraView createViewInstance(ThemedReactContext reactContext) {
+        RCTNodeCameraView view = new RCTNodeCameraView(reactContext);
         return view;
     }
 
-    public RCTNodePlayerView getNodePlayerView() {
-        return view;
+    @ReactProp(name = "outputUrl")
+    public void setOutputUrl(RCTNodeCameraView view, @Nullable String outputUrl) {
+        view.setOutputUrl(outputUrl);
     }
 
-    @ReactProp(name = "inputUrl")
-    public void setInputUrl(RCTNodePlayerView view, @Nullable String inputUrl) {
-        view.setInputUrl(inputUrl);
-    }
-
-    @ReactProp(name = "bufferTime")
-    public void setBufferTime(RCTNodePlayerView view, int bufferTime) {
-        view.setBufferTime(bufferTime);
-    }
-    @ReactProp(name = "maxBufferTime")
-    public void setMaxBufferTime(RCTNodePlayerView view, int bufferTime) {
-        view.setMaxBufferTime(bufferTime);
-    }
-
-    @ReactProp(name = "scaleMode")
-    public void setScaleMode(RCTNodePlayerView view, String mode) {
-        view.setScaleMode(mode);
-    }
-
-    @ReactProp(name = "renderType")
-    public void setRenderType(RCTNodePlayerView view, String type) {
-        view.setRenderType(type);
-    }
-
-    @ReactProp(name = "autoplay")
-    public void setAutoPlay(RCTNodePlayerView view, Boolean autoPlay) {
-        if(autoPlay) {
-            view.autoPlay();
+    @ReactProp(name="autopreview")
+    public void setAutoPreview(RCTNodeCameraView view, @Nullable Boolean autoPreview) {
+        if(autoPreview) {
+            view.audioPreview();
         }
     }
 
-    @ReactProp(name = "hwEnable")
-    public void setHWEnable(RCTNodePlayerView view, Boolean hwEnable) {
-        view.setHWEnable(hwEnable);
+    @ReactProp(name="camera")
+    public void setCameraParam(RCTNodeCameraView view, ReadableMap cameraParam) {
+        view.setCamera(cameraParam.getInt("cameraId"),cameraParam.getBoolean("cameraFrontMirror"));
+    }
+
+    @ReactProp(name = "audio")
+    public void setAudioParam(RCTNodeCameraView view, ReadableMap audioParam) {
+        view.setAudio(audioParam.getInt("bitrate"),audioParam.getInt("profile"),audioParam.getInt("samplerate"));
+    }
+
+    @ReactProp(name = "video")
+    public void setVideoParam(RCTNodeCameraView view, ReadableMap videoParam) {
+        view.setVideo(videoParam.getInt("preset"),videoParam.getInt("fps"),videoParam.getInt("bitrate"),videoParam.getInt("profile"),videoParam.getBoolean("videoFrontMirror"));
+    }
+
+    @ReactProp(name = "denoise")
+    public void setDenoise(RCTNodeCameraView view, boolean denoise) {
+        view.setDenoise(denoise);
+    }
+
+    @ReactProp(name = "smoothSkinLevel")
+    public void setSmoothSkinLevel(RCTNodeCameraView view, int level) {
+        view.setSmoothSkinLevel(level);
     }
 
     @Nullable
     @Override
     public Map<String, Integer> getCommandsMap() {
         return MapBuilder.of(
-                COMMAND_PAUSE_NAME,COMMAND_PAUSE_ID,
+                COMMAND_STARTPREV_NAME,COMMAND_STARTPREV_ID,
+                COMMAND_STOPPREV_NAME,COMMAND_STOPPREV_ID,
                 COMMAND_START_NAME,COMMAND_START_ID,
-                COMMAND_STOP_NAME,COMMAND_STOP_ID
+                COMMAND_STOP_NAME,COMMAND_STOP_ID,
+                COMMAND_SWITCH_CAM_NAME,COMMAND_SWITCH_CAM_ID,
+                COMMAND_SWITCH_FLASH_NAME,COMMAND_SWITCH_FLASH_ID
         );
     }
 
     @Override
-    public void receiveCommand(RCTNodePlayerView root, int commandId, @Nullable ReadableArray args) {
+    public void receiveCommand(RCTNodeCameraView root, int commandId, @Nullable ReadableArray args) {
         switch (commandId) {
-            case COMMAND_PAUSE_ID:
-                root.pause();
+            case COMMAND_STARTPREV_ID:
+                root.startPrev();
+                break;
+            case COMMAND_STOPPREV_ID:
+                root.stopPrev();
                 break;
             case COMMAND_START_ID:
                 root.start();
                 break;
             case COMMAND_STOP_ID:
                 root.stop();
+                break;
+            case COMMAND_SWITCH_CAM_ID:
+                root.switchCam();
+                break;
+            case COMMAND_SWITCH_FLASH_ID:
+                root.setFlashEnable(args.getBoolean(0));
                 break;
         }
     }
